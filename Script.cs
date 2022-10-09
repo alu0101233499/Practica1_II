aa3d5314f264ba1c87aa3f0df07d7e5ab1003c19
Lorenzo Gabriel Pérez González <alu0101233499@ull.edu.es>
2022-10-09 12:25:10 +0100

    Script añadido

---
+++ Script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Personaje");
+        Debug.Log("Coche");
+        Debug.Log("Cubo");
+        Debug.Log("Terreno");
+        Debug.Log("Luz");
+        Debug.Log("Cámara");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
